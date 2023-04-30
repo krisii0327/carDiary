9825d55207127128cd25dabb54b518cfef6653e2
krisii0327 <kovesi44@gmail.com>
2023-04-30 17:11:47 +0200

    register page redirect repair

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,13 +1,13 @@
-import { useContext, useState } from "react";
-import { Link, Navigate } from "react-router-dom";
-import axios from "axios";
-import { UserContext } from "../UserContext";
+import { useContext, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import axios from 'axios';
+import { UserContext } from '../UserContext';
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState(false); // Átirányítást végző useState
-  const {setUser} = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
 
   async function loginUser(ev) {
     //Nem fogja újratölteni az oldal az ev.preventDefault() miatt.
@@ -19,49 +19,74 @@ export default function LoginPage() {
     } catch (error) {
       alert('Login failed!');
     }
-  };
+  }
 
   //Ha login sikeres akkor belépést követően az IndexPage-re írányit
   if (redirect) {
     return <Navigate to={'/'} />;
-  };
+  }
 
   return (
     <div className="mt-4 flex items-center justify-center grow">
       <div className="mb-48 bg-gray-100 px-4 py-4 rounded-2xl border border-black">
-        <h1 className="text-3xl text-center mb-4">
-          Login
-        </h1>
+        <h1 className="text-3xl text-center mb-4">Login</h1>
 
         <form className="max-w-md mx-auto w-80 lg:w-96" onSubmit={loginUser}>
           <div className="flex gap-2 items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+              />
             </svg>
-            <input type="email"
+            <input
+              type="email"
               value={email}
-              onChange={ev => setEmail(ev.target.value)}
-              placeholder="your@example.com" />
+              onChange={(ev) => setEmail(ev.target.value)}
+              placeholder="your@example.com"
+            />
           </div>
           <div className="flex gap-2 items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
+              />
             </svg>
-            <input type="password"
+            <input
+              type="password"
               value={password}
-              onChange={ev => setPassword(ev.target.value)}
-              placeholder="password" />
+              onChange={(ev) => setPassword(ev.target.value)}
+              placeholder="password"
+            />
           </div>
 
           <button className="mt-4 primary">Login</button>
 
           <div className="text-gray-500 py-2 flex gap-1 justify-center">
             <span>Don't have an account yet?</span>
-            <Link className="underline text-black" to="/register">Register now</Link>
+            <Link className="underline text-black" to="/register">
+              Register now
+            </Link>
           </div>
         </form>
-
       </div>
     </div>
-  )
+  );
 }
